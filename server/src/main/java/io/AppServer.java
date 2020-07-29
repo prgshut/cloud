197949c63fe14d8bb40f28404657fb9353bbfd5e
prgshut <prgshut@gmail.ru>
2020-07-29 15:26:19 +0300

    lesson2

--- server/src/main/java/io/AppServer.java
+++ server/src/main/java/io/AppServer.java
@@ -1,3 +1,5 @@
+package io;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
--- server/src/main/java/io/Start.java
+++ server/src/main/java/io/Start.java
@@ -1,3 +1,5 @@
+package io;
+
 public class Start {
     public static void main(String[] args) {
         int port=8189;
